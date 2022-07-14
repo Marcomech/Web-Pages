b4ae46c0b8a9e3be236dea786df80be9c894c04e
marco <mmecha@gmail.com>
2022-07-14 10:36:01 -0300

    Updates

--- Descargar Musica/script.js
+++ Descargar Musica/script.js
@@ -1,18 +1,30 @@
-const form = document.getElementById('form');
-const fsong = document.getElementById('fsong');
+function checkInputs() {
+	const MovieName = fMovie.value;
+	return MovieName.replaceAll(" ","20%");
+}
 
-
+function GetApi(MovieName){
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '7dacbfbe26mshf90acba44fffd95p1ba356jsne52e36e39442',
+			'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+		}
+	};
+	fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${MovieName}`, options)
+		.then(response => response.json())
+		.then(data => {
+			const list = data.d;
+			list.map((item)=>{
+				console.log(item)
+			})
+		})
+		.catch(err => console.error(err))
+	//document.getElementById('resultado').innerHTML = response;
+}
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	
-	checkInputs();
+	MovieName = checkInputs();
+	GetApi(MovieName);
 });
-
-function checkInputs() {
-	const SongName = fsong.value.trim();
-
-	
-	console.log(SongName)
-
-}
